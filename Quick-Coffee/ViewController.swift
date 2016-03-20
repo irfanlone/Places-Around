47b1b93f5d477a84ec49a4ad24cdb7ee4237842b
Irfan <irfan.mf69@gmail.com>
2016-03-19 17:57:26 -0700

    Initial Commit

---
+++ Quick-Coffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quick-Coffee
+//
+//  Created by Irfan Lone on 3/19/16.
+//  Copyright © 2016 Ilone Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
