937d43034ff9db48d5d7a799546a228209613eac
Irfan <irfan.mf69@gmail.com>
2016-03-28 18:36:06 -0700

    code abstraction - added static method to Venues

--- Quick-Coffee/Venue.swift
+++ Quick-Coffee/Venue.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Ilone Labs. All rights reserved.
 //
 
+import Foundation
+
 struct Location {
     
     var address : String!
@@ -57,4 +59,59 @@ struct Venue {
         self.iconUrl = _iconUrl
         self.location = Location.init(_address: _address, _latitude: _lat, _longitude: _lng, _distance: _distance)
     }
+    
+    static func processJsonDataToVenues(json: AnyObject) -> [Venue] {
+        let response = json.valueForKey("response")
+        let venues: NSArray = response?.valueForKey("venues") as! NSArray
+        
+        var list : [Venue] = []
+        
+        for i in 0 ..< venues.count {
+            let venueItem = venues[i] as? NSDictionary
+            let name = venueItem!["name"] as! String
+            
+            var url: String = ""
+            if let websiteUrl = venueItem!["url"] {
+                url = websiteUrl as! String
+            }
+            
+            let location = venueItem!["location"] as! NSDictionary
+            var street : String = ""
+            if let st = location["address"] {
+                street = st as! String
+            }
+            var city : String = ""
+            if let cty = location["city"] {
+                city  = cty as! String
+            }
+            let address = "\(street), \(city)"
+            let distance = location["distance"] as! Float64
+            let latitude = location["lat"] as! Float64
+            let longitude = location["lng"] as! Float64
+            
+            let contact = venueItem!["contact"] as! NSDictionary
+            var phoneNumber : String = ""
+            if let phone = contact["formattedPhone"] {
+                phoneNumber = phone as! String
+            }
+            
+            var menuUrl : String = ""
+            if let menu = venueItem!["menu"] {
+                let dict = menu as! NSDictionary
+                menuUrl = dict["url"] as! String
+            }
+            
+            let identifier = venueItem!["id"] as! String
+            
+            let venueObj = Venue(_id: identifier, _name: name, _address: address, _website: url, _menuUrl:menuUrl, _phone: phoneNumber, _distance: distance, _lat: latitude, _lng: longitude, _iconUrl: "")
+            list.append(venueObj)
+            
+        }
+        // sort the list
+        list.sortInPlace({$0.location.distance < $1.location.distance})
+
+        return list
+    }
+
 }
+
